3a322c2235d24203e2deb6964375a2dd72428140
Michaël Roy <microygh@gmail.com>
2014-03-07 14:36:56 +0100

    Update vimrc.

--- install/install.sh
+++ install/install.sh
@@ -2,26 +2,22 @@
 
 set -e
 
-echo "Copy configuration files..."
+echo 'Copy configuration files...'
 tar xvfJ /usr/share/mr-debian-minimal/mr-debian-minimal.tar.xz -C /
 
-echo "Disable network interfaces in udev..."
+echo 'Disable network interfaces in udev...'
 rm -fv /etc/udev/rules.d/70-persistent-net.rules
 : > /etc/udev/rules.d/75-persistent-net-generator.rules
 
-echo 'Activation de la coloration syntaxique et du fond noir pour vim...'
-sed -i 's/^"syntax on/syntax on/' /etc/vim/vimrc
-sed -i 's/^"set background/set background/' /etc/vim/vimrc
-
-echo "Cleanup /etc/skel..."
+echo 'Cleanup /etc/skel...'
 rm -fv /etc/skel/.profile
 rm -fv /etc/skel/.bashrc
 
-echo "Cleanup /root..."
+echo 'Cleanup /root...'
 rm -fv /root/.bashrc
 rm -fv /root/.profile
 
-echo "Cleanup /home..."
+echo 'Cleanup /home...'
 for USER in `ls /home`
 do
 	rm -fv /home/$USER/.bashrc
@@ -29,4 +25,4 @@ do
 done
 
 
-echo "Done."
+echo 'Done.'
