21ae60f05519f5d70c05feff3d67b9f0987ff67a
Michaël Roy <microygh@gmail.com>
2014-03-20 20:30:55 +0100

    Backup previous configuration files.

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
 #! /bin/bash -e
 
-#
-# Post-installation script for Debian stable
-#
 
-echo "Copy configuration files..."
+echo "~~ Post-installation script for Debian stable ~~"
+
+echo "~ Backup previous configuration files..."
+CONFFILES='/etc/inputrc /etc/apt/sources.list /etc/bash.bashrc /etc/nanorc /etc/skel/.bashrc /etc/skel/.profile'
+for FILE in $CONFFILES; do
+	cp -fv $FILE $FILE~
+done
+
+echo "~ Copy configuration files..."
 cp -rfv config/* /
 
-echo "Disable network interfaces in udev..."
-rm -fv /etc/udev/rules.d/70-persistent-net.rules
-: > /etc/udev/rules.d/75-persistent-net-generator.rules
-
-echo "Remove previous bash configuration files..."
+echo "~ Remove previous bash configuration files..."
 rm -fv /etc/skel/.profile
 rm -fv /etc/skel/.bashrc
 rm -fv /root/.bashrc
@@ -22,10 +23,14 @@ do
 	rm -fv /home/$USER/.profile
 done
 
-echo "Update packages..."
+echo "~ Disable network interfaces in udev..."
+rm -fv /etc/udev/rules.d/70-persistent-net.rules
+: > /etc/udev/rules.d/75-persistent-net-generator.rules
+
+echo "~~ Update packages..."
 apt-get update
 apt-get purge -y --force-yes vim-tiny
 apt-get install -y --force-yes vim tree htop 
 apt-get clean
 
-echo "Done."
+echo "~~ Done."
