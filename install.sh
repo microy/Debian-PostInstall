5ec9cd737bb8e4f9abeecf13699cd07d26bd4b57
Michaël Roy <microygh@gmail.com>
2014-03-22 16:09:00 +0100

    Cleanup comments.

--- install.sh
+++ install.sh
@@ -1,18 +1,19 @@
 #! /bin/bash
 
+#
+# Post-installation script for Debian stable
+#
 
-echo "~~ Post-installation script for Debian stable ~~"
-
-echo "~ Backup previous configuration files..."
+# Backup previous configuration files
 CONFFILES='/etc/inputrc /etc/apt/sources.list /etc/bash.bashrc /etc/nanorc /etc/skel/.bashrc /etc/skel/.profile'
 for FILE in $CONFFILES; do
 	cp -fv $FILE $FILE~
 done
 
-echo "~ Copy configuration files..."
+# Copy configuration files
 cp -rfv config/* /
 
-echo "~ Remove previous bash configuration files..."
+# Remove previous bash configuration files
 rm -fv /etc/skel/.profile
 rm -fv /etc/skel/.bashrc
 rm -fv /root/.bashrc
@@ -23,14 +24,12 @@ do
 	rm -fv /home/$USER/.profile
 done
 
-echo "~ Disable network interfaces in udev..."
+# Disable network interfaces in udev
 rm -fv /etc/udev/rules.d/70-persistent-net.rules
 : > /etc/udev/rules.d/75-persistent-net-generator.rules
 
-echo "~~ Update packages..."
+# Update packages
 apt-get update
 apt-get purge -y --force-yes vim-tiny
 apt-get install -y --force-yes vim tree htop 
 apt-get clean
-
-echo "~~ Done."
