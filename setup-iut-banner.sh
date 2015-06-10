d07582343f1b251ca54e520274a848ea74296962
Michaël Roy <microygh@gmail.com>
2015-06-10 16:52:22 +0200

    Add a script to configure the system banners.

---
+++ setup-iut-banner.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+#
+# Configure the system banners
+#
+
+# Backup previous banners
+cp -fv /etc/issue /etc/issue.backup
+cp -fv /etc/issue.net /etc/issue.net.backup
+cp -fv /etc/motd /etc/motd.backup
+
+# Configure the new banners
+echo "
+Réseaux et Télécoms Auxerre
+Debian GNU/Linux 8 \n \l
+" > /etc/issue
+cp -fv /etc/issue /etc/issue.net
+: > /etc/motd
+
+
