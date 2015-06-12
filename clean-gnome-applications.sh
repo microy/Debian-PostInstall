44e649358b3f0a5807d12b008c9872c0a2408ffd
Michaël Roy <microygh@gmail.com>
2015-06-12 15:18:02 +0200

    Remove more gnome applications.

--- clean-gnome-applications.sh
+++ clean-gnome-applications.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 #
-# Cleanup gnome
+# Remove unnecessary applications in Gnome
 #
 
 apt purge evolution empathy cheese gnome-maps gnome-mahjongg gnome-games \
@@ -12,7 +12,9 @@ gnome-sound-recorder gnome-user-guide gnome-contacts gnome-documents gnome-mines
 gnome-klotski gnome-robots gnome-nibbles aisleriot five-or-more four-in-a-row \
 iagno quadrapassel hitori swell-foop tali xboard lightsoff gnome-packagekit-data \
 gnome-font-viewer gnome-logs gnome-disk-utility gnome-shell-extension-weather \
-bijiben tracker reportbug gnome-nettool yelp baobab gnome-system-log seahorse
+bijiben tracker reportbug gnome-nettool yelp baobab gnome-system-log seahorse \
+gnome-online-miners gnome-accessibility-themes alacarte dconf-editor avahi-daemon \
+rygel 
 
 # Fix meta-package dependencies
 aptitude update
