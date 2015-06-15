9dda7bff97ed8a1d248148649582a0ae969da589
Michaël Roy <microygh@gmail.com>
2015-06-16 01:48:54 +0200

    Add an option to setup apt sources.

--- setup-iut.sh
+++ setup-iut.sh
@@ -1,15 +1,13 @@
 #! /bin/sh
 
 #
-# Configure the system for the Auxerre Institute of Technology
+# Configure the system banner, and apt sources
 #
 
-# Backup previous banners
+# Configure system banner
 cp -afv /etc/issue /etc/issue.$(date "+%Y%m%d")
 cp -afv /etc/issue.net /etc/issue.net.$(date "+%Y%m%d")
 cp -afv /etc/motd /etc/motd.$(date "+%Y%m%d")
-
-# Configure the new banners
 echo 'IUT Réseaux et Télécoms Auxerre
 Debian GNU/Linux 8 \\n \\l
 ' > /etc/issue
@@ -17,13 +15,15 @@ cp -fv /etc/issue /etc/issue.net
 : > /etc/motd
 
 # Setup apt sources to use our local mirror
-cp -afv /etc/apt/sources.list /etc/apt/sources.list.$(date "+%Y%m%d")
-echo '# Debian
-deb http://10.129.44.50/debian jessie main contrib non-free
-# Updates
-deb http://10.129.44.50/debian jessie-updates main contrib non-free
-# Backports
-deb http://10.129.44.50/debian jessie-backports main contrib non-free
-# Security
-deb http://10.129.44.50/security.debian jessie/updates main contrib non-free' > /etc/apt/sources.list
-apt update
+if [ "$1" = "-apt" ]; then
+	cp -afv /etc/apt/sources.list /etc/apt/sources.list.$(date "+%Y%m%d")
+	echo '# Debian
+	deb http://10.129.44.50/debian jessie main contrib non-free
+	# Updates
+	deb http://10.129.44.50/debian jessie-updates main contrib non-free
+	# Backports
+	deb http://10.129.44.50/debian jessie-backports main contrib non-free
+	# Security
+	deb http://10.129.44.50/security.debian jessie/updates main contrib non-free' > /etc/apt/sources.list
+	apt update
+fi
