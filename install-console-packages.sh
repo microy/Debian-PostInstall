14620961aa60f31e83746d02517352e6621d0315
Michaël Roy <microygh@gmail.com>
2015-06-15 11:04:35 +0200

    Remove tshark, and add iptraf, ndisc6, iftop tcpdump...

--- install-console-packages.sh
+++ install-console-packages.sh
@@ -6,4 +6,6 @@
 
 apt update
 apt purge -y --force-yes vim-tiny netcat-traditional
-apt install -y --force-yes tree htop vim mtr-tiny netcat-openbsd netstat-nat iperf ipcalc tshark minicom
+apt install -y --force-yes tree htop vim mtr-tiny netcat-openbsd \
+netstat-nat iperf ipcalc deborphan minicom ndisc6 iftop tcpdump iotop \
+iptraf bmon
