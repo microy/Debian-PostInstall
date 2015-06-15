db663fdd83cb99052f306a18a1bafa872ca65d1a
Michaël Roy <microygh@gmail.com>
2015-06-15 18:25:35 +0200

    Merge setup-bash and setup-console-applications.

--- setup-console-applications.sh
+++ setup-console-applications.sh
@@ -4,19 +4,34 @@
 # Install and configure useful console applications
 #
 
-# Install some applications
+# Install some useful applications
 apt update
 apt purge -y vim-tiny netcat-traditional
 apt install -y tree htop vim mtr-tiny netcat-openbsd \
 netstat-nat iperf ipcalc deborphan minicom ndisc6 iftop tcpdump iotop \
 iptraf bmon
 
-# Configure Nano to highlight the syntax of conf files
+# Configure bash global configuration file
+cp -afv /etc/bash.bashrc /etc/bash.bashrc.$(date "+%Y%m%d")
+cp -fv bash.bashrc /etc/bash.bashrc
+
+# Configure .bashrc files
+cp -afv /root/.bashrc /root/.bashrc.$(date "+%Y%m%d")
+cp -fv .bashrc /root/.bashrc
+cp -fv .bashrc /etc/skel/.bashrc
+for u in $(ls /home); do
+	if [ "$u" = 'lost+found' ]; then continue; fi
+	cp -afv /home/$u/.bashrc /home/$u/.bashrc.$(date "+%Y%m%d")
+	cp -fv .bashrc /home/$u/.bashrc
+	chown $u:$u /home/$u/.bashrc
+done
+
+# Enable conf file syntax highlighting in Nano
 cp -afv /etc/nanorc /etc/nanorc.$(date "+%Y%m%d")
 cp -fv conf.nanorc /usr/share/nano/conf.nanorc
-echo "
+echo '
 ## Configuration files (catch-all syntax)
-include \"/usr/share/nano/conf.nanorc\"" >> /etc/nanorc
+include "/usr/share/nano/conf.nanorc"' >> /etc/nanorc
 
 # Enable syntax highlighting in Vim
 echo '" Enable syntax highlighting by default.
