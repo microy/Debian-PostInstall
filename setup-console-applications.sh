a2d781bad84006011818f805fcb136a7aeb23d2e
Michaël Roy <microygh@gmail.com>
2015-06-15 18:04:39 +0200

    Make a unique script to install and setup useful console applications.

---
+++ setup-console-applications.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+#
+# Install and configure useful console applications
+#
+
+# Install some applications
+apt update
+apt purge -y --force-yes vim-tiny netcat-traditional
+apt install -y --force-yes tree htop vim mtr-tiny netcat-openbsd \
+netstat-nat iperf ipcalc deborphan minicom ndisc6 iftop tcpdump iotop \
+iptraf bmon
+
+# Configure Nano to highlight the syntax of conf files
+cp -afv /etc/nanorc /etc/nanorc.$(date "+%Y%m%d")
+cp -fv conf.nanorc /usr/share/nano/conf.nanorc
+echo "
+## Configuration files (catch-all syntax)
+include \"/usr/share/nano/conf.nanorc\"" >> /etc/nanorc
+
+# Enable syntax highlighting in Vim
+echo '" Enable syntax highlighting by default.
+syntax on' > /etc/vim/vimrc.local
--- setup-vim.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-#
-# Enable syntax highlighting in vim
-#
-
-echo '" Enable syntax highlighting by default.
-syntax on' > /etc/vim/vimrc.local
