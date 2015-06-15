a027b2ffd34f5f23b43719377555f41ff359655b
Michaël Roy <microygh@gmail.com>
2015-06-15 11:05:24 +0200

    Add language options, and lock screen parameter.

--- setup-gnome-desktop.sh
+++ setup-gnome-desktop.sh
@@ -5,10 +5,10 @@
 #
 
 # Configure extensions
-gsettings set org.gnome.shell enabled-extensions "['alternative-status-menu@gnome-shell-extensions.gcampax.github.com', 'apps-menu@gnome-shell-extensions.gcampax.github.com', 'places-menu@gnome-shell-extensions.gcampax.github.com', 'drive-menu@gnome-shell-extensions.gcampax.github.com']"
+gsettings set org.gnome.shell enabled-extensions "['apps-menu@gnome-shell-extensions.gcampax.github.com', 'places-menu@gnome-shell-extensions.gcampax.github.com', 'drive-menu@gnome-shell-extensions.gcampax.github.com']"
 
 # Configure favorite apps
-gsettings set org.gnome.shell favorite-apps "['iceweasel.desktop', 'libreoffice-writer.desktop', 'org.gnome.Nautilus.desktop', 'gnome-terminal.desktop']"
+gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'gnome-terminal.desktop', 'putty.desktop', 'geany.desktop', 'libreoffice-startcenter.desktop', 'iceweasel.desktop', 'chromium.desktop', 'wireshark.desktop', 'virtualbox.desktop', 'packettracer.desktop']"
 
 # Enable desktop icons
 gsettings set org.gnome.desktop.background show-desktop-icons true
@@ -19,6 +19,7 @@ gsettings set org.gnome.shell.calendar show-weekdate true
 
 # Disable lock screen
 gsettings set org.gnome.desktop.screensaver lock-enabled false
+#gsettings set org.gnome.desktop.lockdown disable-lock-screen true
 
 # Disable event sounds
 gsettings set org.gnome.desktop.sound event-sounds false
@@ -33,6 +34,9 @@ gsettings set org.gnome.settings-daemon.plugins.power button-power "shutdown"
 # Configure the background
 gsettings set org.gnome.desktop.background picture-uri file:///usr/share/images/desktop-base/joy.xml
 gsettings set org.gnome.desktop.screensaver picture-uri file:///usr/share/images/desktop-base/joy.xml
+
+# Get rid of the language menu
+gesettings set org.gnome.desktop.input-sources [('xkb', 'fr+latin9')]
 
 # Remove unnecessary folders
 rmdir -v ~/Documents
