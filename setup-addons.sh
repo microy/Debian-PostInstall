88477fd18471abd09e9bbcee3920ae288e5978ee
Michaël Roy <microygh@gmail.com>
2015-06-20 12:59:46 +0200

    Use systemctl. Add external configuration files.

--- setup-addons.sh
+++ setup-addons.sh
@@ -7,55 +7,44 @@
 # Install NTP server
 install_ntp()
 {
-
 	# Install NTP packages
 	apt install -y ntp
 
 	# Configure NTP servers for France
-	sed -i 's/.debian./.fr./' /etc/ntp.conf
+	sed -i 's/^server/#server/' /etc/ntp.conf
+	cat config/ntp.conf >> /etc/ntp.conf
 
 	# Restart NTP service
-	service ntp restart
-
+	systemctl restart ntp
 }
 
 # Install SNMP server
 install_snmp()
 {
-
 	# Install SNMP packages
 	apt install -y snmp snmpd snmp-mibs-downloader
 
 	# Configure SNMP service
 	sed -i 's:export MIBS=:export MIBS=/usr/share/mibs:' /etc/default/snmpd
-	sed -i 's/agentAddress  udp:127/#agentAddress udp:127/' /etc/snmp/snmpd.conf
-	sed -i 's/#agentAddress udp:161/agentAddress  udp:161/' /etc/snmp/snmpd.conf
-	sed -i 's/ rocommunity/#rocommunity/' /etc/snmp/snmpd.conf
-	sed -i '/#rocommunity public  localhost/ a\
-												 #  Full access for all\
- rocommunity public' /etc/snmp/snmpd.conf
+	sed -i 's/^agentAddress/#agentAddress/' /etc/snmp/snmpd.conf
+	sed -i 's/^ rocommunity/#rocommunity/' /etc/snmp/snmpd.conf
+	cat config/snmpd.conf >> /etc/snmp/snmpd.conf
 
 	# Restart SNMP service
-	service snmpd restart
-
+	systemctl restart snmpd
 }
 
 # Install Syslog-Ng server
 install_syslogng()
 {
-
 	# Install packages
 	apt install -y syslog-ng
 
 	# Configure service for external sources
-	echo '# External sources
-	source s_external      { udp(); tcp(); };
-	destination d_external { file("/var/log/external/${HOST}/${YEAR}/${MONTH}/${DAY}/syslog" create_dirs(yes)); };
-	log                    { source(s_external); destination(d_external); };' > /etc/syslog-ng/conf.d/10external-sources.conf
+	cp -fv config/syslog-ng.conf /etc/syslog-ng/conf.d/10external-sources.conf
 
 	# Restart service
-	service syslog-ng restart
-
+	systemctl restart syslog-ng
 }
 
 # Choose what service to install
