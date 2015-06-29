120f317dfc48ff3a2c209e038b4a75c7597bd8b7
Michaël Roy <microygh@gmail.com>
2015-06-29 18:32:48 +0200

    Rename minicom configuration file to its original name.

--- setup-base.sh
+++ setup-base.sh
@@ -31,15 +31,15 @@ for USER in $(ls /home); do
 	rm -fv /home/$USER/.bashrc
 done
 
-# Setup Nano
+# Configure Nano
 cp -fv config/conf.nanorc /usr/share/nano/conf.nanorc
 cp -fv config/nanorc /etc/nanorc
 
-# Setup Vim
+# Configure Vim
 cp -fv config/vimrc /etc/vim/vimrc
 
-# Setup minicom
-cp -fv config/minicom.conf /etc/minicom/minirc.dfl
+# Configure minicom
+cp -fv config/minirc.dfl /etc/minicom/minirc.dfl
 
 # Install VirtualBox guest additions
 if whiptail --title "VirtualBox setup" --yesno "Install VirtualBox guest additions ?" --defaultno 10 50; then
