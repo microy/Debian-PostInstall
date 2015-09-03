d95f4bd48885fe71a67bd6f0eee11dd817df3bcf
root <root@stereovision>
2015-09-03 10:58:08 +0200

    Fix ugly fonts.

--- setup-gnome.sh
+++ setup-gnome.sh
@@ -36,7 +36,8 @@ cp config/*.desktop /usr/share/applications/
 update-desktop-database
 
 # Disable bitmap fonts
-ln -s /usr/share/fontconfig/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/70-no-bitmaps.conf
+ln -sf /usr/share/fontconfig/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/70-no-bitmaps.conf
+cp -fv config/local.conf /etc/fonts/local.conf
 
 # Setup default background
 update-alternatives --set desktop-grub /usr/share/images/desktop-base/joy-grub.png
