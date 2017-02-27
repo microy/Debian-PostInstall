bc7dec6fc4a6b7c8c676a3c69f11dad9d4056782
Michaël Roy <microygh@gmail.com>
2017-02-27 16:38:45 +0100

    Add scripts to install or update Atom and Google Chrome.

--- config/update-atom.sh
+++
@@ -1,8 +0,0 @@
-
-# Function to update Atom
-function update-atom
-{
-  # Download and install Atom
-  wget -O /tmp/atom.deb https://atom.io/download/deb
-  dpkg -i /tmp/atom.deb
-}
--- config/update-chrome.sh
+++
@@ -1,8 +0,0 @@
-
-# Function to update Google Chrome
-function update-chrome
-{
-  # Download and install Google Chrome
-  wget -O /tmp/chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  dpkg -i /tmp/chrome.deb
-}
