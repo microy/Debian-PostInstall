1f528faa2655efd8d3fc94d4c3ec4e500c9596a6
Michaël Roy <microygh@gmail.com>
2017-03-19 14:14:50 +0100

    Backup previous config files.

--- setup-base.sh
+++ setup-base.sh
@@ -4,7 +4,11 @@
 # Base system configuration
 #
 
+# Get the date
+DATE=$(date "+%Y%m%d")
+
 # Setup apt sources in France
+cp -v /etc/apt/sources.list /etc/apt/sources.list.$DATE
 cp -fv config/sources.list.france /etc/apt/sources.list
 
 # Update the package database
@@ -17,27 +21,28 @@ apt purge -y vim-tiny
 apt install -y tree vim git minicom mtr-tiny tcpdump iperf nmap htop
 
 # Configure Bash
+cp -v /etc/bash.bashrc /etc/bash.bashrc.$DATE
 cp -fv config/bash.bashrc /etc/bash.bashrc
 rm -fv /etc/skel/.bashrc
 
+# Remove local bashrc files
+mv -fv /root/.bashrc /root/.bashrc.$DATE
+for USER in $(ls /home); do
+	if [ "$USER" = 'lost+found' ]; then continue; fi
+	mv -fv /home/$USER/.bashrc /home/$USER/.bashrc.$DATE
+done
+
 # Configure Nano
 cp -fv config/conf.nanorc /usr/share/nano/conf.nanorc
+cp -v /etc/nanorc /etc/nanorc.$DATE
 cp -fv config/nanorc /etc/nanorc
 
 # Configure Vim
+cp -fv /etc/vim/vimrc /etc/vim/vimrc.$DATE
 cp -fv config/vimrc /etc/vim/vimrc
 
 # Configure minicom
 cp -fv config/minirc.dfl /etc/minicom/minirc.dfl
-
-# Remove local bashrc files
-if whiptail --title "Bash setup" --yesno "Remove local bashrc files ?" --defaultno 10 50; then
-	rm -fv /root/.bashrc
-	for USER in $(ls /home); do
-		if [ "$USER" = 'lost+found' ]; then continue; fi
-		rm -fv /home/$USER/.bashrc
-	done
-fi
 
 # Install VirtualBox guest additions
 if whiptail --title "VirtualBox setup" --yesno "Install VirtualBox guest additions ?" --defaultno 10 50; then
