0735d33cc163ecd10b08ad5978e5826860c5e1be
Michaël Roy <microygh@gmail.com>
2017-03-30 10:23:40 +0200

    Remove application installation.

--- setup-base.sh
+++ setup-base.sh
@@ -32,18 +32,3 @@ if whiptail --title "GRUB setup" --yesno "Zero GRUB timeout ?" --defaultno 10 50
 	sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/' /etc/default/grub
 	update-grub
 fi
-# Choose applications to install
-APPLICATIONS=$(whiptail --title "Application installation" --separate-output --checklist \
-"Choose additionnal applications to install :" 20 78 16 \
-"minicom" "" OFF "tcpdump" "" OFF "mtr-tiny" "" OFF "git" "" OFF \
-"iperf" "" OFF "ipcalc" "" OFF "ndisc6" "" OFF "netstat-nat" "" OFF \
-"htop" "" OFF "nmap" "" OFF 3>&1 1>&2 2>&3)
-# Install selected applications
-apt install -y $APPLICATIONS
-# Configure selected applications
-for APP in $APPLICATIONS; do
-	case $APP in
-		minicom) cp -fv config/minirc.dfl /etc/minicom/minirc.dfl
-		;;
-	esac
-done
