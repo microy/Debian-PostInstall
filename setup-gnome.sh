54191b07d56fa6f093748307e717ef8a427a0dca
Michaël Roy <microygh@gmail.com>
2017-09-17 05:30:55 +0200

    Setup minimal Gnome.

--- setup-gnome.sh
+++ setup-gnome.sh
@@ -2,52 +2,23 @@
 
 #
 # Remove unnecessary applications in Gnome
-# Install some useful applications
 # Setup Gnome Shell desktop
 #
 
 # Remove unwanted applications
-#apt purge evolution empathy cheese gnome-maps gnome-mahjongg gnome-games \
-#gnome-sudoku gnome-tetravex gnome-chess gnome-dictionary gnome-orca polari \
-#transmission-common simple-scan rhythmbox rhythmbox-data gnome-clocks \
-#hamster-applet gnome-photos gnome-music totem-common gnome-getting-started-docs \
-#gnome-sound-recorder gnome-user-guide gnome-contacts gnome-documents gnome-mines \
-#gnome-klotski gnome-robots gnome-nibbles aisleriot five-or-more four-in-a-row \
-#iagno quadrapassel hitori swell-foop tali xboard lightsoff gnome-packagekit-data \
-#gnome-font-viewer gnome-logs gnome-disk-utility gnome-shell-extension-weather \
-#bijiben tracker reportbug gnome-nettool yelp baobab gnome-system-log seahorse \
-#gnome-online-miners gnome-accessibility-themes dconf-editor avahi-daemon \
-#gnome-color-manager gnome-online-accounts caribou rygel zeitgeist-core
-
 apt purge --yes --autoremove gnome-games gnome-contacts gnome-documents gnome-photos \
 transmission-common gnome-weather gnome-orca gnome-online-accounts gnome-online-miners \
 gnome-calendar gnome-maps gnome-music gnome-sushi polari evolution gnome-dictionary \
 cheese rhythmbox totem gnome-getting-started-docs gnome-sound-recorder simple-scan \
 gnome-clocks reportbug seahorse gnome-software synaptic gnome-disk-utility gnome-logs \
 gnome-characters gnome-color-manager gnome-font-viewer gnome-user-guide gnome-accessibility-themes \
-gnome-orca gnome-online-accounts gnome-online-miners baobab
-
-apt purge --yes --autoremove gnome gnome-games gnome-contacts \
-gnome-disk-utility gnome-user-guide gnome-accessibility-themes \
-gnome-orca gnome-online-accounts gnome-characters gnome-font-viewer \
-gnome-online-miners gnome-software gnome-sushi gnome-logs baobab \
-tracker reportbug avahi-daemon yelp
-
-# Install useful applications
-apt install --yes libreoffice vlc ttf-mscorefonts-installer
-
-#apt install -y gip wireshark ttf-mscorefonts-installer flashplugin-nonfree \
-#geany gparted vlc virtualbox numlockx firefox-esr firefox-esr-l10n-fr
+gnome-orca gnome-online-accounts baobab tracker avahi-daemon yelp
 
 # Configure font rendering
-#ln -sf /usr/share/fontconfig/conf.avail/10-autohint.conf /etc/fonts/conf.d/
-#ln -sf /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d/
-#ln -sf /usr/share/fontconfig/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/
+ln -sf /usr/share/fontconfig/conf.avail/10-autohint.conf /etc/fonts/conf.d/
+ln -sf /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d/
 
 # Setup default background
-#update-alternatives --set desktop-grub /usr/share/images/desktop-base/joy-grub.png
-#update-alternatives --set desktop-background.xml /usr/share/images/desktop-base/joy.xml
-#update-alternatives --set desktop-background /usr/share/images/desktop-base/joy-wallpaper_1920x1200.svg
 update-alternatives --set desktop-grub /usr/share/desktop-base/joy-theme/grub/grub-16x9.png
 update-alternatives --set desktop-background.xml /usr/share/desktop-base/joy-theme/wallpaper/gnome-background.xml
 update-alternatives --set desktop-background /usr/share/desktop-base/joy-theme/wallpaper/contents/images/1920x1200.svg
@@ -57,15 +28,3 @@ dpkg-reconfigure desktop-base
 # FIXME !!!!
 #cp -fv config/gnome.conf /usr/share/glib-2.0/schemas/20_desktop-base.gschema.override
 #glib-compile-schemas /usr/share/glib-2.0/schemas
-
-# Download and install Atom
-#./config/update-atom
-
-# Add script to update Atom
-#cp -fv config/update-atom /usr/local/sbin/
-
-# Download and install Google Chrome
-#./config/update-chrome
-
-# Add script to update Atom
-#cp -fv config/update-chrome /usr/local/sbin/
