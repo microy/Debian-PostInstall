1c3ec8089fbe839d9384afed6e622af6705c5a3f
Michaël Roy <microygh@gmail.com>
2017-09-17 23:53:04 +0200

    Remove firmware, virtualbox, grub setup.

--- setup-base.sh
+++ setup-base.sh
@@ -8,12 +8,10 @@
 DATE=$(date "+%Y%m%d")
 # Setup apt sources in France
 cp -fv /etc/apt/sources.list /etc/apt/sources.list.$DATE
-cp -fv config/sources.list.france /etc/apt/sources.list
+cp -fv config/sources.list /etc/apt/sources.list
 # Update the package database and the system
 apt update
 apt -y upgrade
-# Install Linux firmware
-apt -y install firmware-linux-nonfree
 # Cleanup the packages
 apt -y autoremove
 apt -y autoclean
@@ -29,17 +27,3 @@ for USER in $(ls /home); do
 	if [ "$USER" = 'lost+found' ]; then continue; fi
 	mv -fv /home/$USER/.bashrc /home/$USER/.bashrc.$DATE
 done
-# Install VirtualBox guest additions
-if whiptail --title "VirtualBox setup" --yesno "Install VirtualBox guest additions ?" --defaultno 10 50; then
-#	apt install -y --no-install-recommends virtualbox-guest-dkms linux-headers-amd64
-	apt install -y dkms linux-headers-amd64 build-essential module-assistant
-	m-a prepare
-	mount /media/cdrom
-	cd /media/cdrom
-	sh ./VBoxLinuxAdditions.run
-fi
-# Zero GRUB timeout
-if whiptail --title "GRUB setup" --yesno "Zero GRUB timeout ?" --defaultno 10 50; then
-	sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/' /etc/default/grub
-	update-grub
-fi
