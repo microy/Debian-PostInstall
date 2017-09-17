f41344e58138b82b57a26a68599a2579e8dfc3bf
Michaël Roy <microygh@gmail.com>
2017-09-17 05:32:57 +0200

    Make separate scripts to install Atom and Chrome.

---
+++ install-atom.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+# Download and install Atom
+./config/update-atom
+# Copy the script to update Atom
+cp -fv ./config/update-atom /usr/local/sbin/
---
+++ install-chrome.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+# Download and install Google Chrome
+./config/update-chrome
+# Copy the script to update Google Chrome
+cp -fv ./config/update-chrome /usr/local/sbin/
