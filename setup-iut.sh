a5fb2decf5ab1cf97afbbbd54785dd6f0920097f
Michaël Roy <microygh@gmail.com>
2017-09-18 18:39:24 +0200

    Rework the setup using a whiptail checklist.

--- setup-iut.sh
+++ setup-iut.sh
@@ -8,24 +8,40 @@
 cp -fv config-iut/issue /etc/issue
 cp -fv config-iut/issue.net /etc/issue.net
 rm -fv /etc/motd
-# Setup apt sources to use our local mirror
-if whiptail --title "APT setup" --yesno "Use local APT mirror server ?" --defaultno 10 50; then
+
+# Select additionnal setup
+SETUPIUT=$(whiptail --title "IUT Configuration" --checklist \
+"Choose what to setup :" 20 78 4 \
+"APT" "Use local APT mirror server" OFF \
+"NTP" "Use local NTP time server" OFF \
+"CNTLM" "Use local CNTLM proxy server" OFF \
+"GNOME" "Configure Gnome for IUT" OFF  3>&1 1>&2 2>&3)
+
+# Setup APT
+setup_apt()
+{
 	# Configure APT sources
 	cp -fv config-iut/sources.list /etc/apt/sources.list
 	# Update package database
 	apt update
-fi
+}
+
 # Setup NTP client to use our local time server
-if whiptail --title "NTP setup" --yesno "Use local time server ?" --defaultno 10 50; then
+setup_ntp()
+{
+	# Remove previous server configuration
 	sed -i 's/^NTP=*/#NTP=*/' /etc/systemd/timesyncd.conf
+	# Add our own local NTP server
 	echo "NTP=192.168.5.5" >> /etc/systemd/timesyncd.conf
+	# Use the NTP configuration
 	timedatectl set-ntp true
+	# Restart the NTP client service
 	systemctl restart systemd-timesyncd.service
-	systemctl status systemd-timesyncd.service
-	timedatectl status
-fi
+}
+
 # Setup CNTLM for authentication with our local proxy server
-if whiptail --title "CNTLM setup" --yesno "Use local proxy server ?" --defaultno 10 50; then
+setup_cntlm()
+{
 	# Install CNTLM package
 	apt install -y cntlm
 	# Disable CNTLM service
@@ -36,9 +52,11 @@ if whiptail --title "CNTLM setup" --yesno "Use local proxy server ?" --defaultno
 	# Copy the scripts to start and stop the local proxy
 	cp -fv config-iut/proxy-start /usr/local/sbin/
 	cp -fv config-iut/proxy-stop /usr/local/sbin/
-fi
+}
+
 # Setup Gnome
-if whiptail --title "Gnome setup" --yesno "Configure Gnome for IUT ?" --defaultno 10 50; then
+setup_gnome()
+{
 	# Copy the IUT wallpaper
 	cp -fv config-iut/rt_wallpaper_2013.png /usr/share/wallpapers/
 	# Make the necessary directories for dconf
@@ -53,4 +71,18 @@ if whiptail --title "Gnome setup" --yesno "Configure Gnome for IUT ?" --defaultn
 	cp -fv config-iut/gnome-iut-locks.conf /etc/dconf/db/local.d/locks/
 	# Update dconf
 	dconf update
-fi
+}
+
+# Install selected setups
+for SETUP in $SETUPIUT; do
+	case $SETUP in
+		APT) setup_apt
+		;;
+		NTP) setup_ntp
+		;;
+		CNTLM) setup_cntlm
+		;;
+		GNOME) setup_gnome
+		;;
+	esac
+done
