9fc8b612befc7e1094646d9a3836a3b1a99178af
Michaël Roy <microygh@gmail.com>
2017-09-26 15:47:46 +0200

    Fix a bug in the NTP setup.

--- setup-ntp.sh
+++ setup-ntp.sh
@@ -5,7 +5,7 @@
 #
 
 # Remove previous serveur configuration
-sed -i 's/^NTP=*/#NTP=*/' /etc/systemd/timesyncd.conf
+sed -i 's/^NTP=/#NTP=/' /etc/systemd/timesyncd.conf
 # Add the french NTP servers
 echo "NTP=0.fr.pool.ntp.org 1.fr.pool.ntp.org 2.fr.pool.ntp.org 3.fr.pool.ntp.org" >> /etc/systemd/timesyncd.conf
 # Use the NTP server
