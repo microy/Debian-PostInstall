3c9719f76539f1ab5257aa8af732bb9610307cae
Michaël Roy <microygh@gmail.com>
2017-09-27 22:03:00 +0200

    Setup font rendering.

--- setup-fonts.sh
+++ setup-fonts.sh
@@ -1,5 +1,4 @@
 #! /bin/sh
 
 # Configure font rendering
-ln -sf /usr/share/fontconfig/conf.avail/10-autohint.conf /etc/fonts/conf.d/
-ln -sf /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d/
+cp -fv config/fonts.conf /etc/fonts/local.conf
