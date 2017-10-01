99cb465b2085f21561eca44d79762dc9816c35bf
Michaël Roy <microygh@gmail.com>
2017-10-01 12:08:10 +0200

    Typo.

--- config/show-ip-address.sh
+++ config/show-ip-address.sh
@@ -1,14 +1,18 @@
 #! /bin/sh
 
+# Ignore the sloopback interface
 if [ "$METHOD" = loopback ]; then
     exit 0
 fi
 
-# Only run from ifup.
+# Only run from ifup
 if [ "$MODE" != start ]; then
     exit 0
 fi
 
+# Copy the original login message
 cp /etc/issue-standard /etc/issue
+# Get the network interfaces and their IP addresses,
+# and add them in the login message
 ip -o addr show up primary scope global | awk '{print $2":",$4}' >> /etc/issue
 echo "" >> /etc/issue
