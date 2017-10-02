fea4e57990153b5b3211b477f8e09e6fee42885e
Michaël Roy <microygh@gmail.com>
2017-10-02 18:48:39 +0200

    Fix security: use local variables.

--- iut/config/proxy.sh
+++ iut/config/proxy.sh
@@ -2,16 +2,19 @@
 # Start the proxy
 function proxy-start
 {
+	# Declare local variables
+	local proxy_user=""
+	local proxy_password=""
 	# Read proxy username
 	echo "Proxy authentication..."
 	echo -n "Username: "
-	read PROXY_USER
+	read proxy_user
 	# Read proxy password
 	echo -n "Password: "
-	read -s PROXY_PASSWORD
+	read -s proxy_password
 	# Start CNTLM service
 	echo "Launch the proxy (may need sudo authentication)..."
-	sudo cntlm -u $PROXY_USER -p $PROXY_PASSWORD -d ADAUX 192.168.14.125:8080
+	sudo cntlm -u $proxy_user -p $proxy_password -d ADAUX 192.168.14.125:8080
 	# Export proxy configuration
 	export http_proxy=http://127.0.0.1:3128
 	export https_proxy=http://127.0.0.1:3128
